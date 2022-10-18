e1cfa9cd8bc1b8a08ca71b44f3772930e289cc04
Renan <renan.fachin@hotmail.com>
2022-10-18 11:44:12 -0300

    Modificando a segunda cor do elemento

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import "./css/index.css"
 // Atribuindo à uma constante o HTML com o uso do querySelector
 // > g -> significa que é para pegar o primeiro nível do seletor g
 const primaryCreditCardBgColor = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const secondaryCreditCardBgColor = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 
 // Alterando a color deste path
-primaryCreditCardBgColor.setAttribute("fill", "red")
+primaryCreditCardBgColor.setAttribute("fill", "#C045F5")
+secondaryCreditCardBgColor.setAttribute("fill", "#4572FF")
+
